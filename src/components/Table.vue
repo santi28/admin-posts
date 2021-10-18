d112f092fcbf5f5c30529ac326e7ce3e008470aa
Santiago de Nicolás <santidenicolas@gmail.com>
2021-10-17 21:02:25 -0300

    feat: added table and actions
    Added table with action buttons
    Added delete user action
    Added edit user action
    Added edit user modal

---
+++ src/components/Table.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="table_wrapper">
+
+    <table>
+      <thead>
+        <tr>
+          <th>Nombre</th>
+          <th>Usuario</th>
+          <th>Email</th>
+          <th>Teléfono</th>
+          <th>Website</th>
+          <th>Nombre de la empresa</th>
+          <th>Opciones</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="field in data" :key="field.id">
+          <td>{{field.name}}</td>
+          <td>{{field.username}}</td>
+          <td>{{field.email}}</td>
+          <td>{{field.phone}}</td>
+          <td>{{field.website}}</td>
+          <td>{{field.company.name}}</td>
+          <td class="options">
+            <button
+              @click="$emit('delete', field.id)"
+              class="material-icons">&#xe872;</button>
+            <button
+              @click="$emit('edit', field.id)"
+              class="material-icons-round">&#xe3c9;</button>
+            <button
+              @click="$emit('posts', field.id)"
+              class="material-icons-round">&#xe037;</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    data: Array,
+  },
+};
+</script>
+
+<style scoped>
+.table_wrapper { overflow: auto; }
+
+table {
+  font-family: Poppins;
+  border-radius: 8px;
+  overflow: hidden;
+  border-collapse: collapse;
+  margin: auto;
+  padding:15px;
+  width: 100%;
+}
+
+th {
+  color:#666666;;
+  background:#dddddd;
+
+  border-bottom:1px solid #C1C3D1;
+  padding:15px;
+
+  font-size:1rem;
+  font-weight: 500;
+  text-align:left;
+  vertical-align:middle;
+}
+
+tr {
+  color:#000000;;
+  background:#fafafa;
+
+  border-bottom:1px solid #C1C3D1;
+
+  font-size:1rem;
+  font-weight: 400;
+  text-align:left;
+  vertical-align:middle;
+}
+
+th, tr { white-space: nowrap; width: auto; }
+
+tr:last-child { border-bottom:none; }
+td { padding: 5px 15px; }
+
+.options { user-select: none; }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,139 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-dialog
+      v-model="dialog"
+      persistent
+      max-width="600px">
+      <v-card>
+        <v-card-title><span class="text-h5">Editar Usuario</span></v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <v-col
+                cols="12"
+                sm="6">
+                <v-text-field
+                  v-model="modify.email"
+                  label="Email"></v-text-field>
+              </v-col>
+              <v-col
+                cols="12"
+                sm="6">
+                <v-text-field
+                  v-model="modify.phone"
+                  label="Télefono"></v-text-field>
+              </v-col>
+              <v-col
+                cols="12"
+                sm="6">
+                <v-text-field
+                  v-model="modify.website"
+                  label="Website"></v-text-field>
+              </v-col>
+              <v-col
+                cols="12"
+                sm="6">
+                <v-text-field
+                  v-model="modify.company.name"
+                  label="Nombre de la empresa"></v-text-field>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="black darken-1"
+            text
+            @click="dialog = false">
+            Cerrar
+          </v-btn>
+          <v-btn
+            color="green"
+            text
+            :loading="saveLoading"
+            @click="editUserInformation">
+            Guardar
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+    <Table
+      :data="tableData"
+      @edit="editUser"
+      @delete="deleteUser"/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import axios from 'axios';
+import Table from '../components/Table.vue';
+
+const modifyReset = {
+  id: null,
+  email: null,
+  phone: null,
+  website: null,
+  company: { name: null },
+};
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld,
+    Table,
+  },
+  data: () => ({
+    tableData: [],
+    dialog: false,
+    modify: modifyReset,
+    saveLoading: false,
+  }),
+  methods: {
+    async deleteUser(user) {
+      const res = await axios.delete(`https://jsonplaceholder.typicode.com/users/${user}`);
+      this.tableData = this.tableData.filter((elem) => elem.id !== user);
+      console.log(res);
+    },
+    editUser(user) {
+      this.modify.id = user;
+      this.dialog = true;
+    },
+    async editUserInformation() {
+      this.saveLoading = true;
+      const { id } = this.modify;
+      const i = this.tableData.findIndex((elem) => elem.id === id);
+
+      this.tableData[i].email = this.modify.email || this.tableData[i].email;
+      this.tableData[i].phone = this.modify.phone || this.tableData[i].phone;
+      this.tableData[i].website = this.modify.website || this.tableData[i].website;
+      this.tableData[i].company.name = this.modify.company.name;
+
+      const res = await axios.put(`https://jsonplaceholder.typicode.com/users/${id}`, this.modify);
+      console.log(res);
+
+      this.modify = {
+        id: null,
+        email: null,
+        phone: null,
+        website: null,
+        company: { name: null },
+      };
+
+      this.dialog = false;
+      this.saveLoading = false;
+    },
+  },
+  async mounted() {
+    const res = await axios.get('https://jsonplaceholder.typicode.com/users');
+    this.tableData = res.data;
   },
 };
 </script>
+
+<style scoped>
+  .home {
+    width: 95%;
+    max-width: 1500px;
+    margin-top: 25px;
+  }
+</style>
